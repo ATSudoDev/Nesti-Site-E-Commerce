f62406137e84cc2ca3501305b718d2773363c08f
Axel Teillier <axel.teillier@hotmail.fr>
2021-05-12 17:23:38 +0200

    Revert "Revert "Revert "UPDATE page recipes"""
    This reverts commit cb9e82af1443960d2e0dbd076b64c4ae465f61d4.

--- app/Controllers/TagsController.php
+++ app/Controllers/TagsController.php
@@ -2,33 +2,175 @@
 
 namespace App\Controllers;
 
-use App\Models\RecipeModel;
 use App\Models\TagModel;
-use App\Models\TagHasRecipeModel;
+use App\Entities\Tag;
 
 class TagsController extends BaseController
 {
 
-  public function tag($idTag)
+  public function tags()
   {
-      helper('form');
-      $recipeModel = new RecipeModel();
-      $tagModel = new TagModel();
-      $tagHasRecipeModel = new TagHasRecipeModel();
-      
-      $tags = $tagModel->findAll();
+    helper(['form']);
+    $model = new TagModel();
+    $tags = $model->findAll();
+    return $this->twig->render('tags/all-tags', ['tags' => $tags]);
+  }
 
-      $tagHasRecipes = $tagHasRecipeModel->where("tags_id_tag", $idTag)->findAll();
+  public function tag($id)
+  {
+    $model = new TagModel();
+    $tag = $model->find($id);
+    //where('id_tag', $id)
+    return $this->twig->render('tags/one-tag', ['tag' => $tag]);
+  }
+
+  public function editTag($id)
+  {
+    helper(['form']);
+    $model = new TagModel();
+    $tag = $model->find($id);
+    return $this->twig->render('tags/edit-tag', ['tag' => $tag]);
+  }
+
+  public function deleteTag()
+  {
+    helper(['form']);
+    $model = new TagModel();
+    $id = $this->request->getPost('id_tag');
+    $model->delete($id);
+    return redirect()->to('/tags');
+  }
+
+  public function createTag()
+  {
+    helper(['form']);
+    return $this->twig->render('tags/create-tag');
+  }
+
+  public function insertTag()
+  {
+
+    $session = session();
+
+    // 1. On définie des règles
+    // il faut que les clés correspondent à nom des "input";
+    $rules = [
+      'tag_name' => 'required|alpha|min_length[2]'
+    ];
+
+    if ($this->validate($rules)) {
+
+      // 3. On récupère les données
+      $name = $this->request->getPost('tag_name');
+
+      // 4. On rempli un objet Entity
+      $tag = new Tag();
      
-      $recipes = [];
-      foreach( $tagHasRecipes as $tagHasRecipe){
-        $recipes[] =  $recipeModel->find($tagHasRecipe->recipes_id_recipes);
+      $tag->fill([
+        'tag_name' => ucfirst($name),
+      ]);
+
+      // 5. On fait appel au model
+      $model = new TagModel();
+
+      // 6. On sauvegarde
+      // si l'id est renseigné il fait un update, sinon il fait un insert
+      $s = $model->insert($tag);
+      
+      // 7. On traite les erreurs éventuelles
+      if ($s === false) {
+        // les données enregistrées dans flasData ne sont concervés
+        // que pour la prochaine page, puis elles seront détruites.
+        $session->setFlashdata('errors', $model->errors());
+      } else {
+        $session->setFlashdata('success', true);
+      }
+    }
+    // 8. on redirige ou on affiche une vue
+    return redirect()->to('/tags');
+  }
+
+  public function searchTag()
+  {
+    return $this->twig->render('tags/search-tag');
+  }
+
+  // UPDATE TAG
+  public function updateTag()
+  {
+
+    $session = session();
+
+    // 1. On définie des règles
+    // il faut que les clés correspondent à nom des "input";
+    $rules = [
+      'tag_name' => 'required|alpha|min_length[2]',
+      'id_tag' => 'required|numeric'
+    ];
+
+    // 2. On valide les entrées en faisant appelle à la méthode
+    //$this->validate()
+
+    if ($this->validate($rules)) {
+
+      // 3. On récupère les données
+      $id = $this->request->getPost('id_tag');
+      $name = $this->request->getPost('tag_name');
+
+      // 4. On rempli un objet Entity
+      $tag = new Tag();
+     
+      $tag->fill([
+        'tag_name' => ucfirst($name),
+        'id_tag' => $id,
+        //'slug' => $this->slugify($name)
+        // slugify() est une fonction perso, définie dans Common.php
+      ]);
+
+      // 5. On fait appel au model
+      $model = new TagModel();
+
+      // 6. On sauvegarde
+      // si l'id est renseigné il fait un update, sinon il fait un insert
+      $s = $model->update($id, $tag);
+      
+      // 7. On traite les erreurs éventuelles
+      if ($s === false) {
+        // les données enregistrées dans flasData ne sont concervés
+        // que pour la prochaine page, puis elles seront détruites.
+        $session->setFlashdata('errors', $model->errors());
+      } else {
+        $session->setFlashdata('success', true);
+      }
+    }
+    // 8. on redirige ou on affiche une vue
+    return redirect()->to('/tags');
+  }
+
+  public function slugify($text)
+  {
+      // replace non letter or digits by -
+      $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+      // transliterate
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+      // remove unwanted characters
+      $text = preg_replace('~[^-\w]+~', '', $text);
+
+      // trim
+      $text = trim($text, '-');
+
+      // remove duplicate -
+      $text = preg_replace('~-+~', '-', $text);
+
+      // lowercase
+      $text = strtolower($text);
+
+      if (empty($text)) {
+          return 'n-a';
       }
 
-      $data["loc"] = "recipes";
-      $data["idTag"] = $idTag;
-      $data["recipes"] = $recipes;
-      $data["tags"] = $tags;
-      return $this->twig->render("pages/recipes.html", $data);
+      return $text;
   }
 }
